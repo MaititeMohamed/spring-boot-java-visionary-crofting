90b095908ca2a0d008c483d473132abd258e3a7c
MAITITE MOHAMED <maititemohamed@gmail.com>
2022-11-25 11:00:28 +0100

    initial commit

---
+++ src/main/java/com/youcode/visionarycrofting/VisionarycroftingApplication.java
@@ -0,0 +1,13 @@
+package com.youcode.visionarycrofting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionarycroftingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionarycroftingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youcode/visionarycrofting/VisionarycroftingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youcode.visionarycrofting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionarycroftingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
