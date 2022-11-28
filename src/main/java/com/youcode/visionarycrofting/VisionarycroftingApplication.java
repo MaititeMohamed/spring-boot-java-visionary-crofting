22416584579df664061e1b013702501ab087d83e
Jamal raouj <jamalrwj@gmail.com>
2022-11-28 14:45:55 +0100

    .

--- src/main/java/com/youcode/visionarycrofting/VisionarycroftingApplication.java
+++ src/main/java/com/youcode/visionarycrofting/VisionarycroftingApplication.java
@@ -3,15 +3,14 @@ package com.youcode.visionarycrofting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @RestController
-
+@EnableSwagger2
 public class VisionarycroftingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VisionarycroftingApplication.class, args);
 	}
-
-
 }
